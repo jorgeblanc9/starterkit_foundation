c64cd1305a3527a4621fb55808e45dfa4bcb3fc1
jorgeblanc9 <jorgeblanc9@gmail.com>
2019-04-07 17:12:14 -0400

    Nueva  compilacion sass y browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,17 @@
 var gulp          = require('gulp');
 var browserSync   = require('browser-sync').create();
-var reload        = browserSync.reload;
-var $             = require('gulp-load-plugins');
+var $             = require('gulp-load-plugins')();
 var autoprefixer  = require('autoprefixer');
-var notify        = require('gulp-notify');
-var plumber       = require("gulp-plumber");
 var imagemin      = require("gulp-imagemin");
+var plumber       = require("gulp-plumber");
 
 var sassPaths = [
   'node_modules/foundation-sites/scss',
   'node_modules/motion-ui/src'
 ];
 
-var onError = function(err){
-  console.log("Se ha producido un error: ", err.message);
-  this.emit("end");
-}
-
 function sass() {
-  return gulp.src('scss/**/*.scss')
-    .pipe(plumber({errorHandler:onError}))
+  return gulp.src('scss/app.scss')
     .pipe($.sass({
       includePaths: sassPaths,
       outputStyle: 'compressed' // if css compressed **file size**
@@ -29,10 +21,8 @@ function sass() {
       autoprefixer({ browsers: ['last 2 versions', 'ie >= 9'] })
     ]))
     .pipe(gulp.dest('css'))
-    .pipe(browserSync.stream())
-    .pipe(notify({message: "Tarea Sass finalizada"}));
+    .pipe(browserSync.stream());
 };
-
 
 gulp.task("imagemin", function(){
   return gulp.src("./images/raw/**/*.*")
@@ -46,7 +36,6 @@ gulp.task("imagemin", function(){
           .pipe(notify({message: "Tarea imagemin finalizada"}));
 });
 
-
 function serve() {
 
   var archivos = [
@@ -54,7 +43,8 @@ function serve() {
     './*.php',
     './template-parts/*.php',
     './inc/*.php',
-    './js/**/*.js'
+    './js/**/*.js',
+    'images/**/*.*'
   ]
   browserSync.init( archivos, {
     proxy: "http://localhost/dws",
@@ -62,9 +52,11 @@ function serve() {
   });
 
   gulp.watch("scss/**/*.scss", sass);
-  gulp.watch("images/raw/*.*", imagemin);
+  gulp.watch("", imagemin);
 }
 
 gulp.task('sass', sass);
 gulp.task('serve', gulp.series('sass', serve));
-gulp.task('default', gulp.series(gulp.parallel('sass','imagemin') , serve));
+gulp.task('default', gulp.series('sass', serve));
+
+
